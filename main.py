1dfb3012b2ae1b5cb3173f8903aae25843a8a415
Егор <geo-04@mail.ru>
2023-05-04 12:18:45 +0700

    1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('bam')
+army = 50
+budget = 50
+loyalty = 50
+tech = 50
+ecology = 50
+quests = [] # Список событий
+while(0 < army < 100) and (0 < budget < 100) and (0 < loyalty < 100) and (0 < tech < 100) and (0 < ecology < 100):
+
