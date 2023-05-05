e2334f203d0ca0226ceed4a17fa2305360dee2c6
Evgeny <e.tarlo@g.nsu.ru>
2023-05-05 10:58:57 +0700

    UPD

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+"""
+Title: The game
+
+Group:
+Gagol Egor
+Tarlo Evgeny
+Karpenko Nikolay
+"""
+
 army = 50
 budget = 50
 loyalty = 50
 tech = 50
 ecology = 50
-quests = [] # Список событий
-while(0 < army < 100) and (0 < budget < 100) and (0 < loyalty < 100) and (0 < tech < 100) and (0 < ecology < 100):
+quests = []  # Events list
 
+while(0 < army < 100) and (0 < budget < 100) and (0 < loyalty < 100) and (0 < tech < 100) and (0 < ecology < 100):
