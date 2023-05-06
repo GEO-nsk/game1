6cc5070be2169fdebad0d69bc3711fa37c6ca078
Evgeny <e.tarlo@g.nsu.ru>
2023-05-07 02:06:42 +0700

    ver 0.02

--- Evgeny's test file.py
+++ Evgeny's test file.py
@@ -1,15 +1,26 @@
-import tkinter
+from tkinter import *
 from tkinter import ttk
 
-
-game = tkinter.Tk()  # creating window
+game = Tk()  # creating window
 game.title('The game')  # give name to window
-game.geometry('1920x1080')  # window sizes
-
-btn = ttk.Button(text='Test Click')  # creating test button
-btn.pack()
+game.geometry('1920x1080')
+frame = Frame(game)
+frame.pack(side=LEFT, fill=BOTH, expand=True)
 
 quitButton = ttk.Button(text='Quit', command=game.quit)  # creating quit button
-quitButton.place(x=1650, y=900)  # placing quit button
+quitButton.place(relx=.5, rely=.99, anchor='s')
 
+label1 = Label(frame, text='Привет, это метка!')
+label1.pack()
+
+entry1 = Entry(frame)
+entry1.pack()
+
+for widget in frame.winfo_children():
+    widget.destroy()
+
+label2 = Label(frame, text='Привет, это метка!')
+label2.pack()
+
+game.update()
 game.mainloop()
